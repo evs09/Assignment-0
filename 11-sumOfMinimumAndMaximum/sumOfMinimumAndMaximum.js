58c9e823d4c9b2eb5fbbd2774a6729ba569142d9
Evelyn Velez <67163723+evs09@users.noreply.github.com>
2022-01-03 18:18:17 -0500

    Solution-08-09-10-11

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,23 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
+  let min = Number.MAX_SAFE_INTEGER
+  let max = Number.MIN_SAFE_INTEGER
+
+  let ans = []
+
+  for(let i = 0; i < nums.length; i++){
+    if(nums [i] < min){
+        ans[0] = nums[i]
+        min = nums[i]
+    }
+    if(nums[i] > max){
+        ans[1] = nums[i]
+        max = nums[i]
+    }
+  }
+
+  return (ans[0] + ans[1])
+
 }
 
 // Do not edit this line;
