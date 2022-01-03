08a97208fb055e97ebf0f9f58ac0e9df29aeaee2
Evelyn Velez <67163723+evs09@users.noreply.github.com>
2022-01-03 00:00:52 -0500

    Solution-0-and-1
    Solutions to directories 00 and 01

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,27 @@
 function titleCaseEdit(title) {
   // Insert code here;
+    let str = ""
+	let changeNext = true
+
+	for(let i = 0; i < title.length; i++){
+		if(title.charAt(i) == " "){
+			changeNext = true
+			str += title.charAt(i)
+			continue;
+		}
+
+		if(changeNext){
+			str += title.charAt(i).toUpperCase()
+			changeNext = false;
+		}
+		else{
+			str += title.charAt(i);
+		}
+
+	}
+
+    return str;
+
 }
 
 // Do not edit this line;
